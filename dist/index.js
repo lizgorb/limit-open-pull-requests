d7103a07efbf8e0861eb86083fcc388371c029c4
Liz Gorbonos <liz@akira.md>
2020-05-10 15:41:36 -0400

    rebuild index

--- dist/index.js
+++ dist/index.js
@@ -4207,40 +4207,28 @@ var core = __webpack_require__(470);
 var github = __webpack_require__(469);
 var client_1 = __webpack_require__(976);
 var event_1 = __webpack_require__(352);
+var enforcer_1 = __webpack_require__(973);
 function run() {
     return __awaiter(this, void 0, void 0, function () {
-        var repoToken, _a, owner, repo, client, openPRs, repoLimit, perAuthorLimit, pullRequestNumber_1, triggeringPR_1, openPRsForAuthor, error_1;
+        var triggeringPRNumber, repoToken, repoLimit, perAuthorLimit, _a, owner, repo, client, enforcer, error_1;
         return __generator(this, function (_b) {
             switch (_b.label) {
                 case 0:
                     _b.trys.push([0, 2, , 3]);
-                    if (github.context.eventName !== event_1.Event.PullRequest && github.context.payload.pull_request) {
+                    if (github.context.eventName !== event_1.Event.PullRequest || github.context.payload.pull_request === undefined) {
                         core.setFailed("This action can only be used with pull request events");
                         return [2];
                     }
+                    triggeringPRNumber = github.context.payload.pull_request.number;
                     repoToken = core.getInput('repo-token');
-                    _a = github.context.repo, owner = _a.owner, repo = _a.repo;
-                    client = new client_1.Client(repoToken, owner, repo);
-                    return [4, client.getOpenPullRequests()];
-                case 1:
-                    openPRs = _b.sent();
-                    core.info(JSON.stringify(openPRs));
                     repoLimit = Number(core.getInput('repo-limit'));
                     perAuthorLimit = Number(core.getInput('per-author-limit'));
-                    core.info("Using the following limits: at most " + repoLimit + " open PRs, at most " + perAuthorLimit + " open PRs per author.");
-                    pullRequestNumber_1 = github.context.payload.pull_request.number;
-                    triggeringPR_1 = openPRs.find(function (pr) { return pr.number === pullRequestNumber_1; });
-                    if (triggeringPR_1) {
-                        openPRsForAuthor = openPRs.filter(function (pr) { return pr.author === triggeringPR_1.author; });
-                        core.info("Current open PRs in the repos is " + openPRs.length);
-                        core.info("Current open PRs for " + triggeringPR_1.author + " is " + openPRsForAuthor.length);
-                        if (openPRs.length > repoLimit || openPRsForAuthor.length > perAuthorLimit) {
-                            client.closePullRequest(triggeringPR_1);
-                        }
-                    }
-                    else {
-                        core.info("This action was triggered by a closed pull request. No action will be taken.");
-                    }
+                    _a = github.context.repo, owner = _a.owner, repo = _a.repo;
+                    client = new client_1.Client(repoToken, owner, repo);
+                    enforcer = new enforcer_1.Enforcer(client, { repoLimit: repoLimit, perAuthorLimit: perAuthorLimit }, triggeringPRNumber);
+                    return [4, enforcer.enforceLimits()];
+                case 1:
+                    _b.sent();
                     return [3, 3];
                 case 2:
                     error_1 = _b.sent();
@@ -25482,6 +25470,92 @@ function onceStrict (fn) {
 
 /***/ }),
 
+/***/ 973:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core = __webpack_require__(470);
+var Enforcer = (function () {
+    function Enforcer(client, limits, triggeringPrNumber) {
+        this.client = client;
+        this.limits = limits;
+        this.triggeringPrNumber = triggeringPrNumber;
+    }
+    Enforcer.prototype.enforceLimits = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var openPRs, triggeringPR, openPRsForAuthor;
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4, this.client.getOpenPullRequests()];
+                    case 1:
+                        openPRs = _a.sent();
+                        core.debug(JSON.stringify(openPRs));
+                        core.info("Using the following limits: at most " + this.limits.repoLimit + " open PRs, at most " + this.limits.perAuthorLimit + " open PRs per author");
+                        triggeringPR = openPRs.find(function (pr) { return pr.number === _this.triggeringPrNumber; });
+                        if (!triggeringPR) return [3, 4];
+                        openPRsForAuthor = openPRs.filter(function (pr) { return pr.author === triggeringPR.author; });
+                        core.debug("Current open PRs in the repos is " + openPRs.length);
+                        core.debug("Current open PRs for " + triggeringPR.author + " is " + openPRsForAuthor.length);
+                        if (!(openPRs.length > this.limits.repoLimit || openPRsForAuthor.length > this.limits.perAuthorLimit)) return [3, 3];
+                        return [4, this.client.closePullRequest(triggeringPR, "Sorry, this pull request will be closed. The limit for open pull requests was exceeded.")];
+                    case 2:
+                        _a.sent();
+                        _a.label = 3;
+                    case 3: return [3, 5];
+                    case 4:
+                        core.info("The triggering PR is closed, no action will be taken.");
+                        _a.label = 5;
+                    case 5: return [2];
+                }
+            });
+        });
+    };
+    return Enforcer;
+}());
+exports.Enforcer = Enforcer;
+
+
+/***/ }),
+
 /***/ 976:
 /***/ (function(__unusedmodule, exports, __webpack_require__) {
 
@@ -25558,17 +25632,25 @@ var Client = (function () {
             });
         });
     };
-    Client.prototype.closePullRequest = function (pullrequest) {
+    Client.prototype.closePullRequest = function (pullrequest, comment) {
         return __awaiter(this, void 0, void 0, function () {
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4, this.client.pulls.update({
-                            pull_number: pullrequest.number,
+                    case 0: return [4, this.client.issues.createComment({
+                            issue_number: pullrequest.number,
                             owner: this.repoOwner,
                             repo: this.repoName,
-                            state: pullRequest_1.PullRequestState.Closed
+                            body: comment
                         })];
                     case 1:
+                        _a.sent();
+                        return [4, this.client.pulls.update({
+                                pull_number: pullrequest.number,
+                                owner: this.repoOwner,
+                                repo: this.repoName,
+                                state: pullRequest_1.PullRequestState.Closed
+                            })];
+                    case 2:
                         _a.sent();
                         return [2];
                 }
